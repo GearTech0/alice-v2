38b3d875d8a181acd2efe4a6b09945fa5a0c74c7
Colten Poe <CPLostProgramming@gmail.com>
2020-06-06 13:43:23 -0400

    further clean-up

--- src/commands/classes/Contest.command.ts
+++ src/commands/classes/Contest.command.ts
@@ -1,7 +1,7 @@
-import Command from "./Command";
-import { Message, Channel, TextChannel } from "discord.js";
-import drive from "googleapis";
-import fs, { watchFile } from "fs";
+import Command from './Command';
+import { Message, Channel, TextChannel } from 'discord.js';
+import fs from 'fs';
+import drive from 'googleapis';
 import path from 'path';
 import validUrl from 'valid-url';
 import { dataflow } from "googleapis/build/src/apis/dataflow";
@@ -9,10 +9,12 @@ import contestData from "../../../data/contestData.json";
 import sampleFiles from "../../../templates/sampleFiles.json";
 
 export default class ContestCommand extends Command {
-  public help = "Available Sub-commands for '!Contest': \nStart \nAdd \nVote \nEnd \nHelp "
-  //initiate a contest, announcing it to channel and 
-  //This should read through the google drive folder, and choose N sample files in the root folder and save choice somewhere to be used when viewing list
-  //5 random samples from gdrive, added prior to starting
+  public help = "Available Sub-commands for '!Contest': \nStart \nAdd \nVote \nEnd \nReset \nHelp "
+
+
+  // Initiate a contest, announcing it to channel
+  // This should read through the google drive folder, and choose N sample files in the root folder and save choice somewhere to be used when viewing list
+  // Files to be added prior to starting via !Contest add
   public start(args: Array<string>, message: Message): void{
     if(contestData.contestActive){
       message.reply(`There is already a Contest ongoing. \nCheck <#${contestData.contestChannelId}> to participate!`);
@@ -25,22 +27,19 @@ export default class ContestCommand extends Command {
 
     console.log("Starting Entry Selection");
     try {
-      let obj = {name:"Entry File", url: "www.com", votes: 0};
-      let files = sampleFiles//GDriveController.getFiles();
-      //^^ replacement test code for Gdrive retrieval
-      let pastEntrants = contestData.pastEntries;//JSON.parse(fs.readFileSync(path.join(__dirname, "../../../data/pastEntrants.json")).toString());
+      let files = sampleFiles; // Replace with file Retrieval from GDrive once available
+      let pastEntrants = contestData.pastEntries;
       console.log("pastEntrants initialization:  "+pastEntrants+ "\nEntry 1: "+pastEntrants[0]+"\nEntry2:  "+pastEntrants[1]);
+      
+      
       let entryCount = 5;
-
-      //only needed for reaction voting---
       if(entryCount > reacts.length){  
         let err = new Error();
         err.name = "Reaction Limit Error";
         err.message = "Not enough reactions for chosen entry amount.";
         throw(err);
       }
-      console.log("reaction limit check passed");
-      //---
+      console.log("Reaction limit check passed");
 
       if(entryCount > files.length){
         let err = new Error();
@@ -50,19 +49,17 @@ export default class ContestCommand extends Command {
       }
       console.log("Applicant count check passed");
 
-      //console.log(pastEntrants[0]);
-      for (let x = 0; x < entryCount; x++) {
+      for (let x = 0; x < entryCount; ++x) {
         let id = Math.round(Math.random()*10000)%files.length;
         let fails= 0;
         let failTerm = 1000; //number of times to repick file before throwing error
-        while(  pastEntrants.includes(files[id]) ||  chosenEntries.includes(files[id])   ){ //check if chosen files is a past entrant or is already chosen
+        while(pastEntrants.includes(files[id]) ||  chosenEntries.includes(files[id])){ //check if chosen files is a past entrant or is already chosen
           id = Math.round(Math.random()*10000)%files.length;
           fails++;
           if(fails >= failTerm){
             let err = new Error();
             err.name = "Attempts Count Error"
             err.message = "No valid file found within "+fails+" attempts." ;
-            message.reply('Consider uploading more file!');
             throw(err);           
           }
         }
@@ -76,47 +73,25 @@ export default class ContestCommand extends Command {
     }
     console.log("Successful");
 
-    console.log("Saving entrants to Data...");
-    try{
-      contestData.entries = []
-      for (let entrant of chosenEntries){
-        contestData.entries.push(entrant);
-      }      
-
-    }
-    catch(e){
-     console.error("Error saving contest entrants to ContestData object!: "+e);
-     message.reply(failMes);
-    }
-    console.log("successful");
+    contestData.entries = chosenEntries;
 
     console.log("Writing data to file");
-    try{
-      fs.writeFileSync(path.join(__dirname,"../../../data/contestData.json"),JSON.stringify(contestData, null, 2),{ flag: 'w' });
-      console.log("Contest Data successfully written to file.");
-    }
-    catch(e){
-      console.error("Error writing Contest Data to file!: "+e);
-      message.reply(failMes);
-      return;
-    }
+    
+    fs.writeFileSync(path.join(__dirname,"../../../data/contestData.json"),JSON.stringify(contestData, null, 2),{ flag: 'w' });
+    console.log("Contest Data successfully written to file.");
+    
     console.log("Successful");
 
 
     console.log("Creating announcement string");
     let announcement = "\n:musical_note::musical_note:Its time for another contest!:musical_note::musical_note: ";
     announcement += "\nPlace your vote by clicking the corresponding reaction! \n"
-    try{
-      for(let x=0; x < contestData.entries.length; x++){
-        let entry=contestData.entries[x];
-        announcement += `\n${reacts[x]} ${entry.name}\n${entry.url}\n`;
-      }
+    
+    for(let x=0; x < contestData.entries.length; ++x){
+      let entry=contestData.entries[x];
+      announcement += `\n${reacts[x]} ${entry.name}\n${entry.url}\n`;
     }
-    catch(e){
-      console.error("Error creating message body!: "+e);
-      message.reply(failMes);
-      return;
-    }
+    
     console.log("Successful");
 
     console.log("Determining Server 'contests' channel");
@@ -135,7 +110,7 @@ export default class ContestCommand extends Command {
     let reaction_numbers = ["\u0030\u20E3","\u0031\u20E3","\u0032\u20E3","\u0033\u20E3","\u0034\u20E3","\u0035\u20E3", "\u0036\u20E3","\u0037\u20E3","\u0038\u20E3","\u0039\u20E3"];
     channel.send(announcement).then(function (message: Message){
       contestData.messageId = message.id;
-      for(let x=1; x <= contestData.entries.length; x++){
+      for(let x=1; x <= contestData.entries.length; ++x){
 
         if(x <= 10){
           message.react(reaction_numbers[x]);
@@ -147,8 +122,10 @@ export default class ContestCommand extends Command {
       
       contestData.contestChannelId = message.channel.id;
       fs.writeFileSync(path.join(__dirname,"../../../data/contestData.json"),JSON.stringify(contestData, null, 2),{ flag: 'w' });
+      return;
     }).catch(function (e){
       console.error("Error creating contest message!"+e);
+      return;
     });
     console.log("Successful");
 
@@ -168,8 +145,8 @@ export default class ContestCommand extends Command {
     
   }
 
-  //add to a list of sample files to be used for Contest
-  //can be added to at any time, sounds files to be stored in gdrive
+  // Add to a list of sample files to be used for Contest
+  // Can be added to at any time, sounds files to be stored in gdrive
   public add(args: Array<string>, message: Message): void{
     
     let contestSamples = {samples: []};
@@ -197,43 +174,8 @@ export default class ContestCommand extends Command {
 
   }
 
-  
-  //add vote to selected file in running Contest
-  public vote(args: Array<string>, message: Message): void{
-
-    if(false)/*add check for on going contest*/{return}
-    console.log(args);
-    let votes = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../data/votes.json")).toString());
-    if(!args[0]){
-      var voteList = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../data/votingList.json")).toString());
-      var announcement = "Current Contest Entrants: ";
-      let cnt = [];
-
-      for(let vote in votes){
-
-        cnt[parseInt(votes[vote])-1] = (cnt[parseInt(votes[vote])]-1) ? cnt[parseInt(votes[vote])-1]+1 : 1;
-      }
-
-      for(let x in voteList.samples){
-        announcement += `\n${parseInt(x)+1}: ${voteList.samples[x]} | ${cnt[x]}`;
-      } 
-
-      message.reply(announcement);
-      return;
-    }
-    const user = message.member.user.id;
-    const vote = args[0];
-    if( !((parseInt(vote) <= 5) && (parseInt(vote) > 0) ) ) {message.reply("Invalid vote. Usage: !Contest vote {1-5}"); return}
-    votes[user] = vote;
-    console.log(votes[user]);
-    console.log(JSON.stringify(votes));
-    fs.writeFileSync(path.join(__dirname,"../../../data/votes.json"),JSON.stringify(votes),{ flag: 'w' });
-    message.reply("Vote Accepted");
-    return;
-  }
-
-  //tally vote and announce winner of Contest
-  //top 3 get move to unused entries folder to be reshuffled once all entries have been gone through
+  // Tally vote and announce top 3(can be more in case of ties) as winners of Contest
+  // Winners get saved as Past Entries to be excluded from applicant pool in future contests
   public end(args: Array<string>, message: Message): void{ //
     if(contestData.contestActive){
       console.log("---Ending Contest---");
@@ -245,19 +187,15 @@ export default class ContestCommand extends Command {
       
       console.log("Determining Contest origin message");
       contestChannel.messages.fetch(contestData.messageId).then(contestMessage => {
-        //console.log("ContestMessage ID: "+contestMessage.id+"\t\t"+contestMessage.toString());
         
         let reactions = contestMessage.reactions.cache;
         console.log("Successful");
         let reacEmoji = [];
         let reacCount = [];
-        for (let reaction of reactions)
-        {
+        for (let reaction of reactions){
           reacEmoji.push(reaction[1].emoji);
-          console.log(reaction);
           reacCount.push(reaction[1].count);
-          console.log("reacCount:  " + reacCount);
-
+          if(reacCount.length >= contestData.entries.length){ break; }
         }
 
         console.log("Determining winners");
@@ -272,7 +210,6 @@ export default class ContestCommand extends Command {
             let winner = {};
             let x = reacCount.indexOf(max);
             winner["emoji"] = reacEmoji[x];
-            console.log("saving reacCount: "+reacCount[x]);
             winner["count"] = reacCount[x];
             winner["file"] = files[x];
             files.splice(x,1);
@@ -285,26 +222,25 @@ export default class ContestCommand extends Command {
         console.log("Successful");
 
         console.log("Creating announcement message");
-        //console.log(winners);
         let announcement = "\nThe contest has ended and our winners are: "
-        for(let x=0; x<winners.length; x++){
+        for(let x=0; x<winners.length; ++x){
           announcement += `\n${winners[x].count} votes:   [${winners[x].file.name}]  `;
           let urls = `\n${winners[x].file.url} `;
-          while( (x+1 < winners.length) && (winners[x].count == winners[x+1].count)  ){
-            x++;
+          while((x+1 < winners.length) && (winners[x].count == winners[x+1].count)){
+            ++x;
             announcement += `and  [${winners[x].file.name}]  `;
             urls += `\n\n${winners[x].file.url} `;
           }
           announcement += urls+"\n";
       
         }
-        announcement += "\n :tada:CONGLATIONS:tada: \n\n Remember to upload your files before the next contest!";
+        announcement += "\n :tada:CONGRATULIONS:tada: \n\n Remember to upload your files before the next contest!";
         console.log('Successful');
         
-        for(let x=0; x<winners.length; x++){
+        for(let x=0; x<winners.length; ++x){
           contestData.pastEntries.push(winners[x].file);
         }
-        console.log("Writing to file");
+        console.log("Writing contestData to file");
         contestData.contestActive = false;
         try{
           fs.writeFileSync(path.join(__dirname,"../../../data/contestData.json"),JSON.stringify(contestData, null, 2),{ flag: 'w' });
@@ -316,10 +252,11 @@ export default class ContestCommand extends Command {
 
         contestChannel.send(announcement);
 
-        console.log("Contest Ended");
+        console.log("Contest has been Ended");
         return;
       }).catch(e => {
         console.error("Contest End Failed: "+e);
+        return;
       });
     }
     else{
@@ -335,13 +272,14 @@ export default class ContestCommand extends Command {
     return;
   }
 
-  public resetPastEntrants(){
+  // Clear Past Entrants data to add them back to applicant pool
+  public reset(){
     contestData.pastEntries = [];
     try{
       fs.writeFileSync(path.join(__dirname,"../../../data/contestData.json"),JSON.stringify(contestData, null, 2),{ flag: 'w' });
     }
     catch(e){
-      console.error("Warning! Failure writing contest data to file after !contest end!: "+e);
+      console.error("Warning! Failure writing contest data to file after !contest reset!: "+e);
     }
   }
 
@@ -353,7 +291,6 @@ export default class ContestCommand extends Command {
       let func = args.shift().toLowerCase();
       if(func === "help") { func = "helpAction";}
       try{
-          console.log("func =  " + func + "\nInitiating Command");
           this[func](args, message);
       }
       catch (Error) {
