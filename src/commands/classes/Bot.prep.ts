57532b05d5259fca5814744562f0c4622f2eb4f3
Colten Poe <CPLostProgramming@gmail.com>
2020-06-06 13:42:56 -0400

    correct  fille locations

--- src/commands/classes/Bot.prep.ts
+++ src/commands/classes/Bot.prep.ts
@@ -4,11 +4,13 @@ import path from "path";
 export function filePrep(){
     if(!fs.existsSync(  path.join(__dirname,"../../../data/contestData.json")  )  )
     {
+        console.log("No Contest Data file found, creating from template.");
         fs.copyFileSync(  path.join(__dirname,"../../../templates/contestData.template.json"),  path.join(__dirname,"../../../data/contestData.json")  );
     }
-    if(!fs.existsSync(  path.join(__dirname,"../../../data/auth.json")  )  )
+    if(!fs.existsSync(  path.join(__dirname,"../../../secret/auth.json")  )  )
     {
+        console.log("No bot authorization token file found, creating from template. \nSetup Authorzation token in /secret/auth.json and restart!");
         fs.copyFileSync(  path.join(__dirname,"../../../templates/auth.template.json"),  path.join(__dirname,"../../../secret/auth.json")  );
-        console.error("Setup auth tokens!");
     }
+    return;
 }
