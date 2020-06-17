d1f1eb17d27a612f92d7a595d9a71e71208862de
ALostNPC <CPLostProgramming@gmail.com>
2020-06-16 20:17:54 -0400

    remove unused code

--- src/commands/classes/Contest.command.ts
+++ src/commands/classes/Contest.command.ts
@@ -158,7 +158,7 @@ export default class ContestCommand extends Command {
 
     console.log("Sending announcement");
 
-    let reaction_numbers = ["\u0030\u20E3","\u0031\u20E3","\u0032\u20E3","\u0033\u20E3","\u0034\u20E3","\u0035\u20E3", "\u0036\u20E3","\u0037\u20E3","\u0038\u20E3","\u0039\u20E3", "\uD83D\uDD1F"];  // 0 - 10
+    
     channel.send(announcement, {embed: mEmbed}).then(async function (message: Message){
       
       contestData.messageId = message.id;
